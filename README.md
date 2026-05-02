e86e3d184c4627ee980a01cb851eda876031e32f
Dieter Krause <dieter@debian.dk>
2026-05-02 15:47:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# techdraw
--- init_git.sh
+++ init_git.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/sh
-
+echo "# techdraw" >> README.md
 git init
 echo "dist-newstyle/" >> .gitignore
 echo "*.hi"           >> .gitignore
 echo "*.o"            >> .gitignore
 echo "*.swp"          >> .gitignore
 echo "*.svg"          >> .gitignore # optional 
+
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/dieter-web/techdraw.git
+git push -u origin main
+
+
+
